889b61b9d7ccff80769409c5396994c74f422f48
Julien Durand <julien.durand@alouka.com>
2015-09-02 15:06:05 +0200

    simple decision tree with sex, age and siblings

--- main.py
+++ main.py
@@ -2,19 +2,91 @@ import numpy as np
 import pandas as pd
 
 # Import the Data into the Script
-train = pd.read_csv("./input/train.csv", dtype={"Age": np.float64}, )
-test = pd.read_csv("./input/test.csv", dtype={"Age": np.float64}, )
+train = pd.read_csv("./input/train.csv", dtype={
+                    "Survived": np.bool_,
+                    "Pclass": np.int32,
+                    "Age": np.float64,
+                    "Fare": np.float64},
+                    )
+test = pd.read_csv("./input/test.csv", dtype={
+                   "Age": np.float64,
+                   "Fare": np.float64},
+                   )
 
 # Create the submission file with passengerIDs from the test file
 submission = pd.DataFrame({"PassengerId": test['PassengerId'],
                           "Survived": pd.Series(dtype='int32')})
 
-sex = test['Sex']
-age = test['Age']
+survived = train['Survived']
+sex = train['Sex']
+age = train['Age']
+fare = train['Fare']
+cl = train['Pclass']
+sib = train['SibSp']
 size = len(sex)
 
-# Fill the Data for the survived column, all females live and all males die
-submission.Survived = [1 if sex[i] == 'female' or age[i] < 9.5 else 0 for i in range(0, size)]
+correct = 0
+total = 0
+
+threshold = 12
+for i in range(0, size):
+    if sex[i] == 'male' and age[i] < threshold and sib[i] < 3:
+        total += 1
+        if survived[i]:
+            correct += 1
+
+print(correct/total)
+print(correct)
+print(total)
+
+correct = 0
+total = 0
+for i in range(0, size):
+    if sex[i] == 'male' and age[i] < threshold and sib[i] > 3:
+        total += 1
+        if survived[i]:
+            correct += 1
+
+print(correct/total)
+print(correct)
+print(total)
+
+
+def classify(data):
+    results = []
+    size = len(data)
+    sex = data['Sex']
+    age = data['Age']
+    fare = data['Fare']
+    sib = data['SibSp']
+    cl = data['Pclass']
+
+    for i in range(0, size):
+        survived = 0
+        if sex[i] == 'female':
+            survived = 1
+        else:  # male
+            if age[i] < 13 and sib[i] < 3:
+                survived = 1
+        results.append(survived)
+    return results
+
+
+def score():
+    size = len(train)
+    results = classify(train)
+    total = 0
+    valid = 0
+    for i in range(0, size):
+        total += 1
+        if results[i] == train['Survived'][i]:
+            valid += 1
+    s = valid / total
+    print('Score on training set : ' + str(s))
+
+score()
+
+submission['Survived'] = classify(test)
 
 # Create final submission file
 submission.to_csv("submission.csv", index=False)
